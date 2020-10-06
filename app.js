4952fdf3acd4f3903ceb4f29efc90a3668a5be5d
alexis-12 <alexisml1234@gmail.com>
2020-10-06 15:21:35 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,111 @@
+// function saludar(nombre:string) {
+//     console.log("Hola " + nombre.toUpperCase() );
+// }
+// var wolverine = {
+//     nombre: "Logan"
+// };
+// saludar(wolverine.nombre)
+// let nombre:string = "Alexis"
+// let apellido:string = "Murillo"
+// let edad:number = 20;
+// let texto = `Hola, ${nombre} ${apellido} (${edad})`;
+// function getNombre(){
+//     return "Juanes";
+// }
+// let texto2:string = `${getNombre()}`;
+// console.log(texto2);
+// Funciones con parametros
+//Parametro obligatorio
+// function activar( quien:string){
+//     let mensaje:string;
+//     mensaje = `${quien} activo la batiseñal`;
+//     console.log(mensaje)
+// }
+// activar("Gordon");
+//Parametro por defecto
+// function activar( quien:string, 
+//                   objeto:string = "batiseñal",
+//                   momento?:string){
+//     let mensaje:string;
+//     if(momento){
+//         mensaje = `${quien} activo la ${objeto} en la ${momento}`;
+//     }
+//     else{
+//         mensaje = `${quien} activo la ${objeto}`;
+//     }
+//     console.log(mensaje)
+// }
+// activar("Gordon","batiseñal","en la noche");
+//Funcion de flecha
+// let miFuncion = function(a){
+//     return a;
+// }
+// let miFuncionF = a => a;
+// //-------------------------------------------------
+// let miFuncion2 = function(a:number,b:number){
+//     return a + b;
+// }
+// let miFuncionF2 = (a:number,b:number) => a + b;
+// //----------------------------------------------------
+// let miFuncion3 = function(nombre:string){
+//     nombre = nombre.toUpperCase();
+//     return nombre;
+// }
+// let miFuncion3F = (nombre:string) => {
+//     nombre = nombre.toUpperCase();
+//     return nombre;
+// }
+// //------------------------------------------------------
+// let hulk = {
+//     nombre: "Hulk",
+//     smash(){
+//         setTimeout(()=>console.log(this.nombre + " smash!!"),1500);
+//     }
+// }
+// hulk.smash();
+//------------------------------------------------------------------
+//11.Destructuracion... arreglos
+// let avenger={
+//     nombre:"Tony",
+//     clave:"Iron Man",
+//     poder:"Filantropo millonario con un supertraje"
+// }
+// let {nombre,clave,poder}= avenger;
+// console.log(nombre,clave,poder);
+//Arreglos
+// let avenger:string[]=["Bruce", "Rocket", "Peter"];
+// let [Hulk, Rocket, spidy] = avenger;
+// console.log(Hulk, Rocket, spidy);
+//Interfaces
+// interface Xmen{
+//     nombre:string,
+//     poder:string
+// }
+// function enviarMision(xmen:Xmen){
+//     console.log("Eviando a:" + xmen.nombre);
+// };
+// function enviarCuartel(xmen:Xmen){
+//     console.log("Eviando al cuartel:" + xmen.nombre);
+// };
+// let wolverine:Xmen={
+//     nombre:"Wolverine",
+//     poder:"Rgeneracion"
+// };
+// enviarMision(wolverine);
+// enviarCuartel(wolverine);
+//CLASES
+var Avenger = /** @class */ (function () {
+    function Avenger(nombre, equipo, nombreReal) {
+        this.nombre = undefined;
+        this.equipo = undefined;
+        this.nombreReal = undefined;
+        this.puedePelear = false;
+        this.peleasGanadas = 0;
+        this.nombre = nombre;
+        this.equipo = equipo;
+        this.nombreReal = nombreReal;
+    }
+    return Avenger;
+}());
+var antman = new Avenger("Antman", "Equipo Cap", "Scott");
+console.log(antman);
